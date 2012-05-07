07dbd9c68588eb96c52ab8d1ddcd17956fa572f0
Daniel Robbins <drobbins@funtoo.org>
2012-05-07 16:43:07 +0000

    GPLv2 licensing fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = 'ZenPacks.community.Tru64'
 VERSION = '1.1'
 AUTHOR = 'Jürg Gerber'
-LICENSE = ''
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.Tru64']
 INSTALL_REQUIRES = ['ZenPacks.zenoss.DnsMonitor>=2.0']
